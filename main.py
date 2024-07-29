7999b9ab7f5f4a10664a4194739eeadfc47945e0
alexandra kashina <kashin159@gmail.com>
2024-07-29 20:49:19 +0300

    apgrat and add file

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
+from config import VACANCIES_PATH_JSON
+from src.saving_json import JSONSaver
 from src.utils import user_choice_json
 
 
 def main():
     """ Запуск программы """
-    # user_input = input("Здравствуйте!\n"
-    #                    "В каком формате хотите записать данные?\n"
-    #                    "Если в json формат, введите 1\n"
-    #                    "Если в txt формат, введите 2\n"
-    #                    "Если хотите удалить данные из файла, введите 3\n")
+    user_input = input("Здравствуйте!\n"
+                       "Если вы желаете получить данные из файла нажмите 1\n"
+                       "Если вы желаете удалить данные из файла нажмите 2\n")
 
-    # if user_input == "1":
-    user_choice_json()
-    # elif user_input == "2":
-    #     user_choice_txt()
-    # elif user_input == "3":
-    #     user_input = input("Какой файл вы хотите отчистить?\n"
-    #                        "json-файл, введите 1\n"
-    #                        "txt-файл, введите 2\n")
-    #     if user_input == "1":
-    #         deleter = JSONSaver(VACANCIES_PATH_JSON)
-    #         deleter.del_data()
-    #         print("Данные удалены!")
-    #     elif user_input == "2":
-    #         deleter = TXTSaver(VACANCIES_PATH_TXT)
-    #         deleter.del_data()
-    #         print("Данные удалены!")
-    # return
+    if user_input == "1":
+        user_choice_json()
+    elif user_input == "2":
+        deleter = JSONSaver(VACANCIES_PATH_JSON)
+        deleter.del_data()
+        print("Данные удалены!")
+    return
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+from config import TEST_VACANCIES_PATH_JSON
+from src.job_filter import Vacancy
+from src.saving_json import JSONSaver
+
+
+@pytest.fixture
+def vacancy():
+    return Vacancy("Менеджер по работе с клиентами", "https://hh.ru/vacancy/101709979",
+                   4_000_000, 7_000_000, "Ташкент",
+                   "Опыт работы в продажах обязателен", "Консультирование клиентов")
+
+
+@pytest.fixture()
+def vacancy2():
+    return Vacancy("Менеджер по работе с клиентами", "https://hh.ru/vacancy/101709979",
+                   40_000, 70_000, "Ташкент",
+                   "Опыт работы в продажах обязателен", "Консультирование клиентов")
+
+
+@pytest.fixture()
+def json_saver():
+    return JSONSaver(filename=TEST_VACANCIES_PATH_JSON)
